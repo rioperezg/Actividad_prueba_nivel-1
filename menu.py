9c02d25c29f1387b262f3ca1751b2127e7ea2b77
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-05 19:36:01 +0100

    modificando menu

--- menu.py
+++ menu.py
@@ -10,5 +10,29 @@ def iniciar():
         print("[4] Modificar un vehiculo")
         print("[5] Borrar un vehiculo   ")
         print("[6] Catalogar un vehiculo")
-        print("[6] Cerrar el Gestor     ")
+        print("[7] Cerrar el Gestor     ")
         print("=========================")
+
+        opcion = input("> ")
+
+        if opcion == "1":
+            print("Listando los clientes")
+            break
+
+        # if opcion == 2:
+
+
+        # if opcion == 3:
+
+
+        # if opcion == 4:
+
+
+        # if opcion == 5:
+
+
+        # if opcion == 6:
+
+
+        # if opcion == 7: 
+print(iniciar())                    
