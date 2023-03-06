4540b5e12b3bfbfdac874952dc2168514da0dbe0
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-06 19:13:46 +0100

    modificado Bicicleta,py

--- Bicicleta.py
+++ Bicicleta.py
@@ -1,7 +1,7 @@
 from vehiculo import Vehiculo
 class Bicicleta(Vehiculo):
     def __init__(self, nbast, color, ruedas, tipo):
-        super().__init__(nbast, color, ruedas, tipo)
+        super().__init__(nbast, color, ruedas)
         self.tipo = tipo
     def __str__(self):
         return "La bicicleta es de tipo {}".format(self.tipo)
