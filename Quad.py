887e595e6aee4d886fb08a60d0fd165c585b0aaa
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-06 19:28:43 +0100

    Quad.py rehehcho

--- Quad.py
+++ Quad.py
@@ -2,7 +2,8 @@ from Coche import Coche
 from Bicicleta import Bicicleta
 class Quad(Coche, Bicicleta):
     def __init__(self, nbast, color, ruedas, velocidad, cilindrada, tipo, carga):
-        super().__init__(nbast, color, ruedas, velocidad, cilindrada, tipo)
+        super(Coche).__init__(nbast, color, ruedas, velocidad, cilindrada)
+        super(Bicicleta).__init__(nbast, color, ruedas, tipo)
         self.carga = carga
     def __str__(self):
         return "El Quad tiene una carga de {}".format(self.carga)
