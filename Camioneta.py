594eeb5da4118b037f79e1b30fbdbaba83cbb445
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-06 19:16:30 +0100

    camoneta modificado

--- Camioneta.py
+++ Camioneta.py
@@ -1,7 +1,7 @@
 from Coche import Coche
 class Camioneta(Coche):
     def __init__(self, nbast, color, ruedas, velocidad, cilindrada, carga):
-        super().__init__(nbast, color, ruedas, velocidad, cilindrada, carga)
+        super().__init__(nbast, color, ruedas, velocidad, cilindrada)
         self.carga = carga
     def __str__(self):
         return "La camioneta tiene un carga de {}".format(self.carga)
